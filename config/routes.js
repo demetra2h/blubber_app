17793000c602d41478669b616f8b074ce44d795a
Demetra Haloutsos <demetra2h@gmail.com>
2016-03-04 11:13:26 -0800

    added routers

--- config/routes.js
+++ config/routes.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var router = express.Router();
 //copied from users index, in the modeling app
+// any resource page you require the controller
+var usersController = require("../controllers/users");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 // copied
 /* GET users listing. */
-router.get('/users', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/users', usersController.index);
 
 module.exports = router;
